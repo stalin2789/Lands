33c14bb2b1168f42e50286aeb464e3c7f4891ada
gregorysoft <gregorysoft@hotmail.com>
2018-09-25 13:17:44 -0500

    dia2.2

---
+++ Lands/Lands/ViewModels/LoginViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace Lands.ViewModels
+{
+    class LoginViewModel
+    {
+        #region Properties 
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool IsRunning { get; set; }
+        public bool IsRemember { get; set; }
+        #endregion
+
+        #region Commands
+        public ICommand LoginCommand { get; set; }
+        #endregion
+
+        #region Contructors
+        public LoginViewModel()
+        {
+            this.IsRemember = true;            
+        }
+        #endregion
+    }
+}
--- Lands/Lands/ViewModels/MainViewModel.cs
+++ Lands/Lands/ViewModels/MainViewModel.cs
@@ -7,6 +7,15 @@ namespace Lands.ViewModels
     class MainViewModel
     {
         //Esta es una clase muy importante
+        #region ViewModels
+        public LoginViewModel Login { get; set; }
+        #endregion
 
+        #region Constructors
+        public MainViewModel()
+        {
+            this.Login = new LoginViewModel();
+        }
+        #endregion
     }
 }

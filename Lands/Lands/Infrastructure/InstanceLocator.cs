3f5e76d174144aa5e44af1928341ab8f6302c1d1
gregorysoft <gregorysoft@hotmail.com>
2018-09-24 22:38:51 -0500

    Dia1

---
+++ Lands/Lands/Infrastructure/InstanceLocator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lands.Infrastructure
+{
+    using ViewModels;
+
+    class InstanceLocator
+    {
+        #region Properties
+        public MainViewModel Main { get; set; }
+        #endregion
+        #region Constructors
+        public InstanceLocator()
+        {
+            this.Main = new MainViewModel();
+        }
+        #endregion
+    }
+}
---
+++ Lands/Lands/ViewModels/MainViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lands.ViewModels
+{
+    class MainViewModel
+    {
+        //Esta es una clase muy importante
+
+    }
+}

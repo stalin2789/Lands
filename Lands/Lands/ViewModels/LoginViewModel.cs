00509608fa29f0ad100cbaa9b675d9b92d6c2ea5
gregorysoft <gregorysoft@hotmail.com>
2018-09-25 13:42:58 -0500

    antes de comer

--- Lands/Lands/ViewModels/LoginViewModel.cs
+++ Lands/Lands/ViewModels/LoginViewModel.cs
@@ -1,12 +1,19 @@
-﻿using System;
+﻿using GalaSoft.MvvmLight.Command;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace Lands.ViewModels
 {
-    class LoginViewModel
+    class LoginViewModel : INotifyPropertyChanged
     {
+        #region Events
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion
+
         #region Properties 
         public string Email { get; set; }
         public string Password { get; set; }
@@ -15,13 +22,56 @@ namespace Lands.ViewModels
         #endregion
 
         #region Commands
-        public ICommand LoginCommand { get; set; }
+        public ICommand LoginCommand
+        {
+            get
+            {
+                return new RelayCommand(Login);//Esto es del nuget que agregamos 
+            }
+        }
+
+        private async void Login()
+        {
+            //si no ha digitado el email
+            if (string.IsNullOrEmpty(this.Email))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must enter an email",
+                    "Accept");
+                return;
+            }
+            if (string.IsNullOrEmpty(this.Password))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must enter a password",
+                    "Accept");
+                return;
+            }
+
+            if (this.Email != "stalin2789@yahoo.es" || this.Password != "1234")
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "Email or password incorrect",
+                    "Accept");
+                this.Password = string.Empty;
+                return;
+            }
+
+            await Application.Current.MainPage.DisplayAlert(
+                    "Ok",
+                    "Fuck yeah",
+                    "Accept");
+
+        }
         #endregion
 
         #region Contructors
         public LoginViewModel()
         {
-            this.IsRemember = true;            
+            this.IsRemember = true;
         }
         #endregion
     }
